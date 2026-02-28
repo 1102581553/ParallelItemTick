602828caac0b553fef4158a5cf5f67b0a05e2560
1102581553 <pheyeji@gmail.com>
2026-02-28 08:44:46 +0000

    Update ParallelItemTick.h

--- src/ParallelItemTick.h
+++ src/ParallelItemTick.h
@@ -14,12 +14,14 @@ class Logger;
 namespace parallel_item_tick {
 
 struct Config {
-    int  version    = 1;
-    bool enabled    = true;
-    bool debug      = false;
-    bool stats      = true;
-    int  numThreads = 0;
-    int  batchSize  = 64;
+    int  version           = 1;
+    bool enabled           = true;
+    bool debug             = false;
+    bool stats             = true;
+    int  numThreads        = 0;
+    int  batchSize         = 64;
+    int  mergeInterval     = 5;
+    int  maxMergesPerFrame = 256;
 };
 
 extern Config                          gConfig;
