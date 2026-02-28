ff28126d954bcd1eac1638908b5e48db84feabe6
1102581553 <pheyeji@gmail.com>
2026-02-28 07:08:30 +0000

    Update ParallelItemTick.h

--- src/ParallelItemTick.h
+++ src/ParallelItemTick.h
@@ -3,6 +3,7 @@
 #include <atomic>
 #include <memory>
 #include <string>
+#include <vector>
 
 #include <ll/api/Config.h>
 #include <ll/api/mod/NativeMod.h>
@@ -14,10 +15,12 @@ class Logger;
 namespace parallel_item_tick {
 
 struct Config {
-    int  version = 1;
-    bool enabled = true;
-    bool debug   = false;
-    bool stats   = true;
+    int  version    = 1;
+    bool enabled    = true;
+    bool debug      = false;
+    bool stats      = true;
+    int  numThreads = 0; // 0 = auto (hardware_concurrency - 1, min 2)
+    int  batchSize  = 32;
 };
 
 extern Config                          gConfig;
